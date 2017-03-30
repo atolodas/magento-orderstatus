6e5a315f46068fc9afb2ecba781e58ef385b0308
Marcio Junior <marciofmjr@hotmail.com>
2017-03-30 10:10:15 -0300

    Atualiza nome da tabela

--- sql/orderstatus_setup/mysql4-install-0.0.1.php
+++ sql/orderstatus_setup/mysql4-install-0.0.1.php
@@ -2,8 +2,8 @@
 $installer = $this;
 $installer->startSetup();
 $installer->run("
-	-- DROP TABLE IF EXISTS `orderstatus`;
-	CREATE TABLE `orderstatus` (
+	-- DROP TABLE IF EXISTS `orderstatus_history`;
+	CREATE TABLE `orderstatus_history` (
 		`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
 		`order_id` int(11) DEFAULT NULL,
 		`user` varchar(255) DEFAULT NULL,
